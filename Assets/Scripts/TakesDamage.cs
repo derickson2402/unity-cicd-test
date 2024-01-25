5356a4c5e596384ab6f8effcdfcc98792095b6ed
Dan Erickson <dan@danerick.com>
2024-01-24 22:14:45 -0500

    Add iFrames when taking damage

--- Assets/Scripts/TakesDamage.cs
+++ Assets/Scripts/TakesDamage.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class TakesDamage : MonoBehaviour
 {
     public float maxHP;             // Max # of health object can have
+    public bool invincible;         // If true the player will not take damage
     public bool isEnemy;            // Is this object considered an enemy or a player (used by DealsDamage)
     public int iFrames;             // Number of invincibility frames after taking damage
-    public float curHP;             // Current number of hit points left
     [SerializeField] protected AudioClip damageSoundEffect;
     [SerializeField] protected AudioClip deathSoundEffect;
+
+    private float curHP;            // Current number of health points, < maxHP
+    private int curIFrames;         // Number of frames invincibility has been active for
+    private bool iFrameActive;     // Are invincibility frames active?
 
     // TODO: allow configuration of special thing to do on death
 
@@ -20,9 +24,33 @@ public class TakesDamage : MonoBehaviour
         curHP = maxHP;
     }
 
+    void Update()
+    {
+        // Count iframes and turn them off when finished
+        if (iFrameActive)
+        {
+            if (curIFrames >= iFrames)
+            {
+                // Turn off iFrames
+                iFrameActive = false;
+            }
+            ++curIFrames;
+        }
+    }
+
+    // Returns the number of health points the object has
+    public float GetHP()
+    {
+        return curHP;
+    }
+
     // Deal damage to the object, decreasing its health, killing it if it goes below 0
     public void Damage(float damagePoints)
     {
+        if (invincible || iFrameActive)
+        {
+            return;
+        }
         curHP -= damagePoints;
         if (curHP <= 0)
         {
@@ -33,6 +61,8 @@ public class TakesDamage : MonoBehaviour
         } else {
             AudioSource.PlayClipAtPoint(damageSoundEffect, Camera.main.transform.position);
         }
+        iFrameActive = true;
+        curIFrames = 0;
     }
 
     // Heal the object, increasing its health (up to healthPoints which is maximum)
