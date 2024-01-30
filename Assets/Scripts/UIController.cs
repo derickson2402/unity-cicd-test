33aa6a3027973a0b5c13a04368aa2142a733076e
The-Big-K <kad747@gmail.com>
2024-01-30 18:56:08 -0500

    Weapon UI.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [SerializeField] protected Image weaponAImage;
+    [SerializeField] protected Image weaponBImage;
+
+
+    // Start is called before the first frame update
+    void setWeaponA()
+    {
+
+    }
+}
