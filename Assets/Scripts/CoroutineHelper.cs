a964a44b7246b3d50b622a697d9c0280437a2863
Dan Erickson <dan@danerick.com>
2024-01-31 10:29:00 -0500

    Remove broken unused imports

--- Assets/Scripts/CoroutineHelper.cs
+++ Assets/Scripts/CoroutineHelper.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class CoroutineHelper {
     public static IEnumerator MoveObjectOverTime(Transform item, Vector3 startingPosition, Vector3 desiredPosition, float duration)
