cdfa1874548a96cbe3bef127f6381472b91c8f8a
Dan Erickson <dan@danerick.com>
2024-02-04 19:56:43 -0500

    Update drop rates, add item drop override, update items on map

--- Assets/Scripts/CharacterMechanics/DropsItemOnDeath.cs
+++ Assets/Scripts/CharacterMechanics/DropsItemOnDeath.cs
@@ -5,11 +5,44 @@ using UnityEngine.Assertions;
 
 public class DropsItemOnDeath : MonoBehaviour
 {
+    public GameObject guranteedDropItem;    // If given, character will drop this on death, otherwise random item
+    public int dropRate;                    // On average, drop item every dropRate times
+
+    private GameObject obj;                 // private member of instantiated guranteedDropItem
+
+    private void Start()
+    {
+        if (guranteedDropItem != null)
+        {
+            obj = Instantiate(guranteedDropItem, transform.position, Quaternion.identity);
+            // Disable colliders on the object so we can control its position
+            BoxCollider bc = obj.GetComponent<BoxCollider>();
+            if (bc != null)
+            {
+                bc.enabled = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (obj != null)
+        {
+            obj.transform.position = transform.position;
+        }
+    }
+
     // Randomly drop an object based on given parameters
     public void OnDestroy()
     {
+        // Check if we have an existing item to drop, re-enable it
+        if (guranteedDropItem != null)
+        {
+            obj.GetComponent<BoxCollider>().enabled = true;
+            return;
+        }
         // Determine whether we drop anything at all
-        if (Random.Range(0, 3) != 0)
+        if (Random.Range(0, dropRate) != 0 || (dropRate < 1))
         {
             return;
         }
